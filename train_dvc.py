caf1b39a7567d226e17fb7ef14333cb87058aee1
Dar1na29 <78657862+Dar1na29@users.noreply.github.com>
2023-04-25 11:56:32 +0300

    edit train_dvc for correct reading parameters

--- train_dvc.py
+++ train_dvc.py
@@ -2,14 +2,10 @@ import argparse
 import os
 import pickle
 import tensorflow as tf
-
+import yaml
 # Define command line arguments
 parser = argparse.ArgumentParser(description='Train a model.')
 
-parser.add_argument('--layer1_size', type=int, default=64,
-                    help='Size of the first hidden layer')
-parser.add_argument('--layer2_size', type=int, default=32,
-                    help='Size of the second hidden layer')
 parser.add_argument('--train_data', type=str, default='data/train.pkl',
                     help='Path to the training data')
 parser.add_argument('--valid_data', type=str, default='data/valid.pkl',
@@ -20,6 +16,15 @@ parser.add_argument('--model_path', type=str, default='model.h5',
 # Parse command line arguments
 args = parser.parse_args()
 
+
+with open('params.yaml', 'r') as f:
+    params = yaml.safe_load(f)
+
+print(params['layer1_size'])
+print(params['layer2_size'])
+
+
+
 # Load the training and validation data
 with open(args.train_data, 'rb') as f:
     train_data = pickle.load(f)
@@ -28,8 +33,8 @@ with open(args.valid_data, 'rb') as f:
 
 # Define the model architecture
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(args.layer1_size, activation='relu', input_shape=(train_data['inputs'].shape[1],)),
-    tf.keras.layers.Dense(args.layer2_size, activation='relu'),
+    tf.keras.layers.Dense(params['layer1_size'], activation='relu', input_shape=(train_data['inputs'].shape[1],)),
+    tf.keras.layers.Dense(params['layer2_size'], activation='relu'),
     tf.keras.layers.Dense(1)
 ])
 
@@ -41,3 +46,4 @@ model.fit(train_data['inputs'], train_data['targets'], validation_data=(valid_da
 
 # Save the model
 model.save(args.model_path)
+
